2fc492e59373abe5d47b31b10b24adeca3f929e1
Ben Ryves <benjamin.ryves@getyourguide.com>
2021-07-06 13:44:22 +0200

    [V8] cleanup errorneous refactor

--- gaql/tooling/autocomplete_generator.py
+++ gaql/tooling/autocomplete_generator.py
@@ -1,4 +1,4 @@
-"""Script for maintaining entities.json"""
+"""Script for maintaining autocomplete.py"""
 from gaql.lib.click_decorators.state import State
 from gaql.lib.google_clients.config import setup_client
 from gaql.lib.google_clients.queries import google_fields_query
