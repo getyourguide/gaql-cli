7629611ac2c17ac38cc9350046b3655f01d492ad
Konstantinos Karvounis <konstantinos.karvounis@getyourguide.com>
2025-03-13 17:21:53 +0100

    Update Google Ads API to v19

--- gaql/lib/google_clients/config.py
+++ gaql/lib/google_clients/config.py
@@ -74,8 +74,8 @@ def setup_client():
 
 
 def setup_ads_service(client):
-    return client.get_service("GoogleAdsService", version="v17")
+    return client.get_service("GoogleAdsService", version="v19")
 
 
 def setup_fields_service(client):
-    return client.get_service("GoogleAdsFieldService", version="v17")
+    return client.get_service("GoogleAdsFieldService", version="v19")
